e568fd4874bba8d3804037458dac78df274e6651
Harsh-neigah <neigahharsh@gmail.com>
2025-08-10 00:37:58 +0530

    Video 5

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
-          >
+          > 
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
